6ea5bf98339183e690fa29de11747ef27ace8c92
Ithilhin <pc.roques@laposte.net>
2024-06-19 13:17:30 +0200

    API platform : installation + expose entities

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -5,8 +5,10 @@ namespace App\Entity;
 use App\Repository\ClientRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[ApiResource]
 class Client
 {
     #[ORM\Id]
--- src/Entity/Prestation.php
+++ src/Entity/Prestation.php
@@ -6,8 +6,10 @@ use App\Repository\PrestationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: PrestationRepository::class)]
+#[ApiResource]
 class Prestation
 {
     #[ORM\Id]
