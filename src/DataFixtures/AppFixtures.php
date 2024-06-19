99cb582de3a08324983ad6a002a9b214be9f1ae1
Ithilhin <pc.roques@laposte.net>
2024-06-19 12:56:19 +0200

    doctrine: hash user password

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,26 +4,40 @@ namespace App\DataFixtures;
 
 use App\Entity\Client;
 use App\Entity\Contact;
-// use App\Entity\User;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * Encodeur de mot de passe
+     * @var UserPasswordHasherInterface
+     */
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }   
+
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create('fr_FR');
 
-        // for ($u = 0; $u < 10; $u++) {
-        //     $user = new User();
-        //     $user->setFirstname($faker->firstName);
-        //     $user->setLastname($faker->lastName);
-        //     $user->setEmail($faker->email);
-        //     $user->setTel($faker->phoneNumber);
-        //     $user->setAdmin(false);
-        //     $user->setActive(true);
-        //     $manager->persist($user);
-        // }
+        for ($u = 0; $u < 10; $u++) {
+            $user = new User();
+            $hash = $this->encoder->hashPassword($user, 'password');
+
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setTel($faker->phoneNumber);
+            $user->setPassword($hash);
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+        }
 
         for ($c = 0; $c < 10; $c++) {
             $client = new Client();
