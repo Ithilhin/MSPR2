cad6e74abff4e003e12d358c763a2397cdc66e4a
Ithilhin <pc.roques@laposte.net>
2024-06-20 14:54:46 +0200

    Configure operations + data validation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,11 @@ use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\Validator\Constraints as SecurityAssert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
@@ -19,43 +24,59 @@ use Symfony\Component\Serializer\Annotation\Groups;
 // Configuration de la ressource API
 // #[ApiResource(paginationEnabled: true, paginationItemsPerPage: 50, order: ['email' => 'ASC'])]
 #[ApiResource(
-    normalizationContext: ['groups' => ['users_read']],
-    denormalizationContext: ['groups' => ['users_write']],
+    // normalizationContext: ['groups' => ['users_read']],
+    // denormalizationContext: ['groups' => ['users_write']],
+    // operations: [
+    //     new Get(
+    //         uriTemplate: '/utilisateurs/{id}'
+    //     ),
+    //     new GetCollection()
+    // ]
 )]
 // #[ApiFilter(SearchFilter::class, properties: ['firstName' => 'partial'])]
 // #[ApiFilter(SearchFilter::class)]
 // #[ApiFilter(OrderFilter::class)]
-
+#[UniqueEntity(fields: ['email'], message: 'Cet email est déjà utilisé')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['users_read'])]
+    // #[Groups(['users_read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 180)]
+    #[Assert\NotBlank(message: 'Veuillez saisir une adresse email')]
+    #[Assert\Email(message: 'Veuillez saisir une adresse email valide')]
     private ?string $email = null;
 
     /**
      * @var list<string> The user roles
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message:'Le rôle est obligatoire')]
     private array $roles = [];
 
     /**
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message:'Veuillez saisir un mot de passe')]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:'Veuillez saisir un prénom')]
+    #[Assert\Length(min: 2,max:255,minMessage: 'Le prénom doit contenir au moins 2 caractères', maxMessage: 'Le prénom doit contenir au maximum 255 caractères')]
     private ?string $firstName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:'Veuillez saisir un nom')]
+    #[Assert\Length(min: 2,max:255,minMessage: 'Le nom doit contenir au moins 2 caractères', maxMessage: 'Le nom doit contenir au maximum 255 caractères')]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:'Veuillez saisir un numéro de téléphone')]
     private ?string $tel = null;
 
     #[ORM\Column(length: 255, nullable: true)]
