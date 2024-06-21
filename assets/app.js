ecb1acdc4e35b5589b57733bd746cb3d153e082e
Ithilhin <pc.roques@laposte.net>
2024-06-21 18:22:31 +0200

    Installation et configuration React

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,15 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react'
+import { createRoot } from 'react-dom/client';
 import './styles/app.css';
+
+export default function App() {
+    return (
+      <h1>Bonjour à tous</h1>
+    )
+  }
+
+  const container = document.querySelector('#app');
+  const root = createRoot(container);
+  root.render(<App />);
+    
+  
