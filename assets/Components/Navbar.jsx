c94ee8dce7a06e3c3ea4d9e3aad136a439425e72
Ithilhin <pc.roques@laposte.net>
2024-06-25 13:06:42 +0200

    gestion de la deconnexion

--- assets/Components/Navbar.jsx
+++ assets/Components/Navbar.jsx
@@ -1,51 +1,81 @@
-import React from 'react';
-
+import React from "react";
+import authAPI from "../Services/authAPI";
 
 export default function Navbar() {
+  const handleLogout = () => {
+    authAPI.logout();
+  };
+
   return (
     <>
-    <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+      <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarColor01">
-                <ul className="navbar-nav m-auto">
-                    <li className="nav-item">
-                    {/* TODO: inserer logo Canopées */}
-                    <a className="nav-link active" href="#">IMG CANOPEES
-                        <span className="visually-hidden">(current)</span>
-                    </a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Qui sommes-nous?</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Prestations</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Tarifs</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Contact</a>
-                    </li>
-                </ul>
-                {/* TODO: aligner les bouttons */}
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link">Inscription</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="btn btn-success">Connexion</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="#" className="btn btn-danger">Deconnexion</a>
-                    </li>
-                </ul>
-            </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav m-auto">
+              <li className="nav-item">
+                {/* TODO: inserer logo Canopées */}
+                <a className="nav-link active" href="#">
+                  IMG CANOPEES
+                  <span className="visually-hidden">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Qui sommes-nous?
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Prestations
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Tarifs
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+            {/* TODO: aligner les bouttons */}
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  Inscription
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="btn btn-success">
+                  Connexion
+                </a>
+              </li>
+              <li className="nav-item">
+                <button
+                  onClick={handleLogout}
+                  href="#"
+                  className="btn btn-danger"
+                >
+                  Deconnexion
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-</nav>
+      </nav>
     </>
-  )
+  );
 }
-
--- assets/Services/authAPI.js
+++ assets/Services/authAPI.js
@@ -1,20 +1,26 @@
 import axios from "axios";
 
+function logout() {
+  window.localStorage.removeItem("authToken");
+  delete axios.defaults.headers["Authorization"];
+}
+
 // Function to authenticate user with given credentials
 function authenticate(credentials) {
   // Sending a POST request to the login endpoint
-  return axios
-    .post("http://localhost:8000/api/login_check", credentials)
-    // Extracting the token from the response
-    .then((response) => response.data.token)
-    // Storing the token in localStorage and setting the Authorization header
-    .then((token) => {
-      window.localStorage.setItem("authToken", token); // Storing token in localStorage
+  return (
+    axios
+      .post("http://localhost:8000/api/login_check", credentials)
+      // Extracting the token from the response
+      .then((response) => response.data.token)
+      // Storing the token in localStorage and setting the Authorization header
+      .then((token) => {
+        window.localStorage.setItem("authToken", token); // Storing token in localStorage
 
-      axios.defaults.headers["Authorization"] = "Bearer " + token; // Setting Authorization header
-    return true // Indicating successful authentication
-    });
+        axios.defaults.headers["Authorization"] = "Bearer " + token; // Setting Authorization header
+      })
+  );
 }
 
 // Exporting the authenticate function for use in other parts of the application
-export default { authenticate };
+export default { authenticate, logout };
